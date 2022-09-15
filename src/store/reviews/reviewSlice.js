4e581480fc9e70aa494f13cde8d08030e94b5e36
unknown <Gordon Hook@SC0-8.digital.lcl>
2022-09-15 19:44:25 +0300

    fix reducer

--- src/store/reviews/reviewSlice.js
+++ src/store/reviews/reviewSlice.js
@@ -1,18 +1,23 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import { db } from "../../components/selectors/db";
 
+export const addNewReviews = createAsyncThunk(
+    'Reviews/addNewReviews',
+)
 const reviewsSlice = createSlice({
     name: 'Reviews',
     initialState: {
+        isLoading: false,
         arrReviews: [],
+        users: [],
     },
-
     reducers: {
         createReview(state, action) {
             state.arrReviews = action.payload
         },
         handleAddNewReview(state, action) {
-            
+            state.isLoading = true
+            state.arrReviews = action.payload
             db.reviews.add({ 
                 name : action.payload.name,
                 review: action.payload.review,
@@ -20,12 +25,26 @@ const reviewsSlice = createSlice({
                 pathLocation: action.payload.pathLocation,
                 date: action.payload.date
             })
+            
         },
         handleDeleteReview(state, action) {
             db.reviews.delete(state.id = action.payload.id)
             db.reviews.toArray();  
         }
+    },
+    extraReducers:{
+        [addNewReviews.pending]: (state) => {
+            state.isLoading = true
+        },
+        [addNewReviews.fulfilled]: (state, action) => {
+            
+        },
+        [addNewReviews.rejected]: (state, action) => {
+
+        }
     }
 })
+
+
 export const { createReview,handleDeleteReview, handleAddNewReview } = reviewsSlice.actions
 export default reviewsSlice.reducer
