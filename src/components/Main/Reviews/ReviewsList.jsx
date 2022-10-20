739b00dfd1b3561936d2bd3a4133231df7d415f2
unknown <Gordon Hook@SC0-8.digital.lcl>
2022-10-20 19:00:04 +0300

    spredsheets

--- src/components/Main/Reviews/ReviewsList.jsx
+++ src/components/Main/Reviews/ReviewsList.jsx
@@ -1,20 +1,30 @@
+import { useEffect, useState } from 'react';
 import styles from './style.module.scss';
-import { useLiveQuery } from 'dexie-react-hooks'
-import { db } from '../../selectors/db'
+// import { useLiveQuery } from 'dexie-react-hooks'
+// import { db } from '../../selectors/db'
 import { Rating } from 'react-simple-star-rating';
 import { useAuth0 } from '@auth0/auth0-react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { handleDeleteReview } from '../../../store/reviews/reviewSlice';
+import axios from 'axios';
 
 export default function ReviewsList() {
-    const reviewsList = useLiveQuery(() => db?.reviews?.toArray())
+    // const reviewsList = useLiveQuery(() => db?.reviews?.toArray())
     const dispatch = useDispatch()
-    const test = useSelector((state) => state.reviews.arrReviews)
-    console.log(test)
+    const [APIdata, setAPIdata] = useState([])
+    // const test = useSelector((state) => state.reviews.arrReviews)
+    // console.log(test)
     const { user } = useAuth0()
+    useEffect(() => {
+        axios.get(`https://sheet.best/api/sheets/b3a38273-5d35-499b-bfca-a5d93b6ad2e1`)
+        .then((incomingData) => {
+            setAPIdata(incomingData.data)
+        })
+    },[])
+    console.log(APIdata)
     return (
         <div className={styles.reviews_block}>
-            {reviewsList?.filter((item) => item.pathLocation === window.location.pathname)?.map(({name,id, review, date, rating}, index) => (
+            {APIdata?.filter((item) => item.pathLocation === window.location.pathname)?.map(({name,id, review, date, rating}, index) => (
             <div key={index} className={styles.review_list}>
                 <span className={styles.avatar}>{name[0]}</span>
                 <span className={styles.name_user}>{name}</span>
